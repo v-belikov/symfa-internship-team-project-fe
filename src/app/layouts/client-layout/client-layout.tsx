b1f9d0c99a83b8e6508a5f8e025a469760d799d4
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-03-16 15:09:25 +0300

    feature/INT-5805: feat: client layout

---
+++ src/app/layouts/client-layout/client-layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { StyleProvider } from '@ant-design/cssinjs';
+import { Layout, Menu } from 'antd';
+import { Content, Header } from 'antd/es/layout/layout';
+import Sider from 'antd/es/layout/Sider';
+
+import './styles.scss';
+
+export const ClientLayout: React.FC = () => {
+  return (
+    <StyleProvider hashPriority="high">
+      <Layout className="client-layout">
+        <Sider>
+          <Menu className="client-layout-menu" />
+        </Sider>
+
+        <Layout className="client-layout-wrapper">
+          <Header className="header" />
+          <Layout className="content">
+            <Content>
+              <Outlet />
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    </StyleProvider>
+  );
+};
