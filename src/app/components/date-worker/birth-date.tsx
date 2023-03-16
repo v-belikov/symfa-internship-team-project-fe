7265bf44a29ec519ba6327753bd97757030afd29
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-03-16 10:48:34 +0300

    feature/INT-5189: feat: add types

--- src/app/components/date-worker/birth-date.tsx
+++ src/app/components/date-worker/birth-date.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import dayjs from 'dayjs';
 
-export const BirthDate = ({ date }: any) => {
+export const BirthDate = ({ date }: any): React.ReactElement<Date> => {
   return <div>{dayjs(date).format('D MMMM YYYY')}</div>;
 };
--- src/app/components/date-worker/submitted-date.tsx
+++ src/app/components/date-worker/submitted-date.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import dayjs from 'dayjs';
 
-export const SubmittedDate = ({ date }: any) => {
+export const SubmittedDate = ({ date }: any): React.ReactElement<Date> => {
   return <div>{dayjs(date).format('DD/MM/YY-h A')}</div>;
 };
