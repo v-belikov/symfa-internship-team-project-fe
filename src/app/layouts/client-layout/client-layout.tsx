4cd0699e087b003a6c5699bb9de7438906c39154
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-03-16 16:32:41 +0300

    feature/INT-5805: fix: client-layout.tsx

--- src/app/layouts/client-layout/client-layout.tsx
+++ src/app/layouts/client-layout/client-layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { StyleProvider } from '@ant-design/cssinjs';
 import { Layout, Menu } from 'antd';
 import { Content, Header } from 'antd/es/layout/layout';
 import Sider from 'antd/es/layout/Sider';
@@ -9,23 +8,19 @@ import './styles.scss';
 
 export const ClientLayout: React.FC = () => {
   return (
-    <StyleProvider hashPriority="high">
-      <Layout className="client-layout">
-        <Sider>
-          <Menu className="client-layout-menu">
-            <Outlet />
-          </Menu>
-        </Sider>
+    <Layout className="client-layout">
+      <Sider>
+        <Menu className="client-layout-menu" />
+      </Sider>
 
-        <Layout className="client-layout-wrapper">
-          <Header className="header" />
-          <Layout className="content">
-            <Content>
-              <Outlet />
-            </Content>
-          </Layout>
+      <Layout className="client-layout-wrapper">
+        <Header className="header" />
+        <Layout className="content">
+          <Content>
+            <Outlet />
+          </Content>
         </Layout>
       </Layout>
-    </StyleProvider>
+    </Layout>
   );
 };
