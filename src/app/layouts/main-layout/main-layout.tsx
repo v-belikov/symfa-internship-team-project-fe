ea1b5abafcee19e5ad8f3e242bf4056d6330cfd4
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-03-19 21:49:09 +0300

    feature/INT-5525: fix: firxed ui kit

--- src/app/layouts/main-layout/main-layout.tsx
+++ src/app/layouts/main-layout/main-layout.tsx
@@ -1,14 +1,6 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import { CoursesBlock } from '@components/courses';
-import { DatabaseTable } from '@components/database';
 
 export const MainLayout: FC = () => {
-  return (
-    <>
-      <DatabaseTable />
-      <Outlet />
-      <CoursesBlock />
-    </>
-  );
+  return <Outlet />;
 };
